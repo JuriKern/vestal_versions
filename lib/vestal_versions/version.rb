6f76e206c78ea3cc8b3f7cafb50796992d3b2285
Juri Semjonov <semjonow@Juris-iMac.local>
2012-09-11 12:43:25 +0300

    Added attr_accessible to version model

--- lib/vestal_versions/version.rb
+++ lib/vestal_versions/version.rb
@@ -10,6 +10,8 @@ module VestalVersions
     # Associate polymorphically with the parent record.
     belongs_to :versioned, :polymorphic => true
 
+    attr_accessible :modifications, :number, :tag
+
     # ActiveRecord::Base#changes is an existing method, so before serializing the +changes+ column,
     # the existing +changes+ method is undefined. The overridden +changes+ method pertained to
     # dirty attributes, but will not affect the partial updates functionality as that's based on
